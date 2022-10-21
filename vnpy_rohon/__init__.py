480de6800a93412d15cf032718d6e5e5818a9298
vn.py <xiaoyou.chen@foxmail.com>
2022-10-21 16:04:23 +0800

    [Mod] 调整RohonGateway对象加载方式

--- vnpy_rohon/__init__.py
+++ vnpy_rohon/__init__.py
@@ -20,13 +20,9 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-try:
-    from .gateway import RohonGateway
-except ImportError:
-    pass
-
-
 import importlib_metadata
+
+from .gateway import RohonGateway
 
 
 try:
